173983e502191731c12ac07823a862429dc4bac2
Haruki <haruki@example.com>
2023-01-26 21:42:41 +0900

    just say Welcome on top page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,12 @@
 import React from "react";
-import { GetServerSideProps } from "next";
 import Layout from "../components/Layout";
 
 const Blog: React.FC = (props) => {
   return (
     <Layout>
-      <div>{JSON.stringify(props)}</div>
+      <div>Welcome To Smash Record Archive</div>
     </Layout>
   );
-};
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const res = await fetch("http://localhost:3100/api/players/5353");
-  const feed = await res.json();
-  return {
-    props: { feed },
-  };
 };
 
 export default Blog;
