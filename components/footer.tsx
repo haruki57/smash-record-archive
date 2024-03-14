c5f3d3b048f320d3d6d11fc45f0ad1138259f369
Haruki Nagasawa <hnagasawa57@gmail.com>
2024-03-14 22:35:15 +0900

    Top page (under construction)

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,31 +2,22 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer>
-      <hr />
-      <div className="mx-auto flex max-w-7xl items-center justify-center py-6 px-8 text-gray-500">
-        <div>
-          Developed By{" "}
-          <a
-            href="https://twitter.com/harukisb"
-            className="text-blue-400"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            @harukisb
-          </a>
+    <footer className="w-full ">
+      <hr className="w-full" />
+      <div className="flex justify-center">
+        <div className="mx-auto max-w-7xl py-6 px-8 text-gray-500">
+          <div>
+            Developed By{" "}
+            <a
+              href="https://twitter.com/harukisb"
+              className="text-blue-400"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @harukisb
+            </a>
+          </div>
         </div>
-        {/* <div className="flex lg:flex-1 font-bold">
-          <Link href="/">Smash Record Archive</Link>
-        </div>
-        <div className="flex gap-x-12">
-          <Link href="/tournaments" className="text-m leading-6">
-            トーナメント一覧
-          </Link>
-          <Link href="/about" className="text-m leading-6">
-            About
-          </Link>
-        </div> */}
       </div>
     </footer>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,127 +1,109 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import useSWR from "swr";
+import { useMemo, useState } from "react";
+import Footer from "@/components/footer";
+import Link from "next/link";
+import Layout from "@/components/layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const fetcher = (...args: any) => fetch(args).then((res) => res.text());
 
 export default function Home() {
-  const { data, error } = useSWR("/master.txt", fetcher);
-  if (!data) {
+  // Tsv
+  // playerId \t playerName \t aliases
+  // aliases can be empty
+  // Players are ordered by number of wins.
+  const { data: playerDataTsv, error } = useSWR("/master.txt", fetcher);
+  const [text, setText] = useState("");
+  const playerData = useMemo(() => {
+    if (!playerDataTsv) {
+      return [];
+    }
+    const ret = [] as {
+      playerId: number;
+      playerName: string;
+      aliases: string;
+    }[];
+    const rows = playerDataTsv.split("\n");
+    rows.forEach((row) => {
+      const splitted = row.split("\t");
+      const [playerId, playerName, aliases] = splitted;
+      ret.push({
+        playerId: Number(playerId),
+        playerName,
+        aliases,
+      });
+    });
+    return ret;
+  }, [playerDataTsv]);
+
+  const matchedPlayers = useMemo(() => {
+    return playerData
+      .filter((p) => {
+        const playerStr = (p.playerName + p.aliases).toLowerCase();
+        return playerStr.indexOf(text.toLowerCase()) >= 0;
+      })
+      .slice(0, 100)
+      .map((p) => {
+        return { playerId: p.playerId, playerName: p.playerName };
+      });
+  }, [text, playerData]);
+
+  if (!playerData) {
     return;
   }
-
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div>{data}</div>
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <main className="mx-auto flex min-h-screen flex-col items-center">
+        <div className="w-full py-32 bg-gray-100 flex justify-center">
+          <div>
+            <div className="mx-full">
+              <input
+                type="text"
+                value={text}
+                placeholder="プレイヤー名で検索"
+                onChange={(e) => setText(e.target.value)}
+                className="w-60 rounded border-2"
+              />
+            </div>
+
+            <div className="overflow-auto overflow-y-scroll h-28">
+              {text.length > 0 && matchedPlayers.length > 0 && (
+                <div className="p-2 border rounded">
+                  {matchedPlayers.map((p) => {
+                    return (
+                      <div key={p.playerId}>
+                        <Link href={`/players/${p.playerId}`}>
+                          {p.playerName}
+                        </Link>
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
+            </div>
+          </div>
+          {/* <div>{text}</div> */}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div>
+          <div>Featured Players</div>
+          <div className="flex justify-beetween">
+            <div>
+              <Link href="/players/5355">HIKARU</Link>
+            </div>
+            <div>
+              <Link href="/players/5355">HIKARU</Link>
+            </div>
+            <div>
+              <Link href="/players/5355">HIKARU</Link>
+            </div>
+          </div>
+        </div>
+        {/* <div>{JSON.stringify(playerData)}</div> */}
+      </main>
+      <Footer />
+    </>
   );
 }
